ba25099e20b7afbec64419bf584eeefa7c73cf39
HarvesteR <felipe@floatingoriginstudios.com>
2022-05-12 17:23:31 -0500

    More tweaks, after reverting the xml thing, the plugin DOES export nearly EVERYTHING!!

--- Mapper/MapperLoading.cs
+++ Mapper/MapperLoading.cs
@@ -1,111 +1,115 @@
 ﻿using ColossalFramework;
 using ColossalFramework.Plugins;
 using ColossalFramework.UI;
+
 using ICities;
+
 using Mapper.Panels;
 using Mapper.Utilities;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
+
 using UnityEngine;
 
 namespace Mapper
 {
-    public class MapperLoading : LoadingExtensionBase
-    {
-        private GameObject exportPanelGameObject;
-        private GameObject whatsNewPanelGameObject;
-        private GameObject buttonObject;
-        private GameObject buttonObject2;
-        private UIButton menuButton;
+	public class MapperLoading : LoadingExtensionBase
+	{
+		private GameObject exportPanelGameObject;
+		//private GameObject whatsNewPanelGameObject;
+		private GameObject buttonObject;
+		private GameObject buttonObject2;
+		private UIButton menuButton;
 
-        private ExportPanel exportPanel;
-        private WhatsNewPanel whatsNewPanel;
-        private LoadMode lastLoadMode_;
+		private ExportPanel exportPanel;
+		//private WhatsNewPanel whatsNewPanel;
+		private LoadMode lastLoadMode_;
 
-        public override void OnLevelLoaded(LoadMode mode)
-        {
-            if (mode != LoadMode.LoadGame && mode != LoadMode.NewGame && mode != LoadMode.NewMap && mode != LoadMode.LoadMap)
-                return;
+		public override void OnLevelLoaded(LoadMode mode)
+		{
+			if (mode != LoadMode.LoadGame && mode != LoadMode.NewGame && mode != LoadMode.NewMap && mode != LoadMode.LoadMap)
+				return;
 
-            lastLoadMode_ = mode;
+			lastLoadMode_ = mode;
 
-            UIView view = UIView.GetAView();
-            UITabstrip tabStrip = null;
+			UIView view = UIView.GetAView();
+			UITabstrip tabStrip = null;
 
-            whatsNewPanelGameObject = new GameObject("whatsNewPanel");
-            this.whatsNewPanel = whatsNewPanelGameObject.AddComponent<WhatsNewPanel>();
-            this.whatsNewPanel.transform.parent = view.transform;
-            this.whatsNewPanel.Hide();
+			//whatsNewPanelGameObject = new GameObject("whatsNewPanel");
+			//this.whatsNewPanel = whatsNewPanelGameObject.AddComponent<WhatsNewPanel>();
+			//this.whatsNewPanel.transform.parent = view.transform;
+			//this.whatsNewPanel.Hide();
 
-            exportPanelGameObject = new GameObject("exportPanel");
-            this.exportPanel = exportPanelGameObject.AddComponent<ExportPanel>();
-            this.exportPanel.transform.parent = view.transform;
-            this.exportPanel.whatsNewPanel = whatsNewPanel;
-            this.exportPanel.Hide();
+			exportPanelGameObject = new GameObject("exportPanel");
+			this.exportPanel = exportPanelGameObject.AddComponent<ExportPanel>();
+			this.exportPanel.transform.parent = view.transform;
+			//this.exportPanel.whatsNewPanel = whatsNewPanel;
+			this.exportPanel.Hide();
 
-            if (mode == LoadMode.NewGame || mode == LoadMode.LoadGame)
-            {
-                tabStrip =  ToolsModifierControl.mainToolbar.component as UITabstrip;
-            }
-            else
-            {
-                tabStrip = UIView.Find<UITabstrip>("MainToolstrip");
-            }
+			if (mode == LoadMode.NewGame || mode == LoadMode.LoadGame)
+			{
+				tabStrip = ToolsModifierControl.mainToolbar.component as UITabstrip;
+			}
+			else
+			{
+				tabStrip = UIView.Find<UITabstrip>("MainToolstrip");
+			}
 
-            buttonObject = UITemplateManager.GetAsGameObject("MainToolbarButtonTemplate");
-            buttonObject2 = UITemplateManager.GetAsGameObject("ScrollablePanelTemplate");
-            menuButton = tabStrip.AddTab("cimtographerMod", buttonObject, buttonObject2, new Type[] { }) as UIButton;
-            menuButton.eventClick += uiButton_eventClick;
-        }
+			buttonObject = UITemplateManager.GetAsGameObject("MainToolbarButtonTemplate");
+			buttonObject2 = UITemplateManager.GetAsGameObject("ScrollablePanelTemplate");
+			menuButton = tabStrip.AddTab("cimtographerMod", buttonObject, buttonObject2, new Type[] { }) as UIButton;
+			menuButton.eventClick += uiButton_eventClick;
+		}
 
-        private void uiButton_eventClick(UIComponent component, UIMouseEventParameter eventParam)
-        {
+		private void uiButton_eventClick(UIComponent component, UIMouseEventParameter eventParam)
+		{
 
-            if (!this.exportPanel.isVisible)
-            {
-                this.exportPanel.isVisible = true;
-                this.exportPanel.BringToFront();
-                this.exportPanel.Show();
+			if (!this.exportPanel.isVisible)
+			{
+				this.exportPanel.isVisible = true;
+				this.exportPanel.BringToFront();
+				this.exportPanel.Show();
 
-                if(this.exportPanel.scrollOptionsList != null)
-                {
-                    this.exportPanel.scrollOptionsList.DisplayAt(0);
-                    this.exportPanel.scrollOptionsList.selectedIndex = 0;
-                }
-            }
-            else
-            {
-                this.exportPanel.isVisible = false;
-                this.exportPanel.Hide();
-            }            
-        }
+				if (this.exportPanel.scrollOptionsList != null)
+				{
+					this.exportPanel.scrollOptionsList.DisplayAt(0);
+					this.exportPanel.scrollOptionsList.selectedIndex = 0;
+				}
+			}
+			else
+			{
+				this.exportPanel.isVisible = false;
+				this.exportPanel.Hide();
+			}
+		}
 
-        public override void OnLevelUnloading()
-        {
-            if (lastLoadMode_ != LoadMode.LoadGame && lastLoadMode_ != LoadMode.NewGame && lastLoadMode_ != LoadMode.NewMap && lastLoadMode_ != LoadMode.LoadMap)
-                return;
+		public override void OnLevelUnloading()
+		{
+			if (lastLoadMode_ != LoadMode.LoadGame && lastLoadMode_ != LoadMode.NewGame && lastLoadMode_ != LoadMode.NewMap && lastLoadMode_ != LoadMode.LoadMap)
+				return;
 
 
-            if (exportPanelGameObject != null)
-            {
-                GameObject.Destroy(exportPanelGameObject);
-            }
+			if (exportPanelGameObject != null)
+			{
+				GameObject.Destroy(exportPanelGameObject);
+			}
 
-            if(whatsNewPanelGameObject != null)
-            {
-                GameObject.Destroy(whatsNewPanelGameObject);
-            }
+			//if (whatsNewPanelGameObject != null)
+			//{
+			//	GameObject.Destroy(whatsNewPanelGameObject);
+			//}
 
-            if (buttonObject != null)
-            {
-                GameObject.Destroy(buttonObject);
-                GameObject.Destroy(buttonObject2);
-                UIComponent.Destroy(menuButton);
-            }
-        }
+			if (buttonObject != null)
+			{
+				GameObject.Destroy(buttonObject);
+				GameObject.Destroy(buttonObject2);
+				UIComponent.Destroy(menuButton);
+			}
+		}
 
-       
-    }
+
+	}
 }
 
--- Mapper/Panels/ExportPanel.cs
+++ Mapper/Panels/ExportPanel.cs
@@ -4,213 +4,212 @@ using Mapper.Managers;
 using System.Collections.Generic;
 using Mapper.CustomUI;
 using Mapper.OSM;
-using ColossalFramework.Steamworks;
 using Mapper.Utilities;
 
 namespace Mapper.Panels
 {
-    class ExportPanel : UIPanel
-    {
-        private int titleOffset = 40;
-        private Vector2 offset = Vector2.zero;
+	class ExportPanel : UIPanel
+	{
+		private int titleOffset = 40;
+		private Vector2 offset = Vector2.zero;
 
-        public WhatsNewPanel whatsNewPanel = null;
-        public UIFastList scrollOptionsList = null;
-        private UIButton buttonGenerate = null;
+		public WhatsNewPanel whatsNewPanel = null;
+		public UIFastList scrollOptionsList = null;
+		private UIButton buttonGenerate = null;
 
-        public override void Awake()
-        {
-            this.isInteractive = true;
-            this.enabled = true;
-            this.width = 400;
-            this.height = 400;
+		public override void Awake()
+		{
+			this.isInteractive = true;
+			this.enabled = true;
+			this.width = 400;
+			this.height = 400;
 
-            base.Awake();
-        }
+			base.Awake();
+		}
 
-        public override void Start()
-        {
-            base.Start();
-            
-            UILabel labelTitle = this.AddUIComponent<UILabel>();
-            labelTitle.text = "Cimtographer";
-            labelTitle.textScale = 1.3f;
-            labelTitle.autoSize = true;
-            labelTitle.relativePosition = new Vector3(4, 4);
+		public override void Start()
+		{
+			base.Start();
 
-            CreateTopMessage();
-            CreateLeftPanel();
-            CreateRightPanel();
-            CreateBottomMessage();
+			UILabel labelTitle = this.AddUIComponent<UILabel>();
+			labelTitle.text = "Cimtographer";
+			labelTitle.textScale = 1.3f;
+			labelTitle.autoSize = true;
+			labelTitle.relativePosition = new Vector3(4, 4);
 
-            this.relativePosition = new Vector3(Mathf.Floor((GetUIView().fixedWidth - width) / 2), Mathf.Floor((GetUIView().fixedHeight - height) / 2));
-            this.backgroundSprite = "UnlockingPanel2";
-            this.atlas = CustomUI.UIUtils.GetAtlas("Ingame");
+			CreateTopMessage();
+			CreateLeftPanel();
+			CreateRightPanel();
+			CreateBottomMessage();
 
-        }
+			this.relativePosition = new Vector3(Mathf.Floor((GetUIView().fixedWidth - width) / 2), Mathf.Floor((GetUIView().fixedHeight - height) / 2));
+			this.backgroundSprite = "UnlockingPanel2";
+			this.atlas = CustomUI.UIUtils.GetAtlas("Ingame");
 
-        private void CreateTopMessage()
-        {
-            UISprite infoSprite = this.AddUIComponent<UISprite>();
-            infoSprite.spriteName = "NotificationIconExtremelyHappy";
-            infoSprite.size = new Vector2(32, 32);
-            infoSprite.relativePosition = new Vector3(4, 40);
+		}
 
-            UILabel label = this.AddUIComponent<UILabel>();
-            label.text =    "Oooo, shiny! Cimtographer's had a re-write, and should now work a\n" +
-                            "lot better than it did before. Check out the <color#94c6ff>workshop page</color> for\n" +
-                            "more information.";
-            label.textScale = 0.6f;
-            label.size = new Vector2(600, 40);
-            label.width = 600;
-            label.relativePosition = new Vector2(40, 40);
-            label.textColor = new Color32(200, 245, 130, 255);
-            label.processMarkup = true;
-            label.eventClicked += TopLabel_eventClicked;
+		private void CreateTopMessage()
+		{
+			UISprite infoSprite = this.AddUIComponent<UISprite>();
+			infoSprite.spriteName = "NotificationIconExtremelyHappy";
+			infoSprite.size = new Vector2(32, 32);
+			infoSprite.relativePosition = new Vector3(4, 40);
 
-            titleOffset += 50;
-        }
+			UILabel label = this.AddUIComponent<UILabel>();
+			//label.text = "Oooo, shiny! Cimtographer's had a re-write, and should now work a\n" +
+			//				"lot better than it did before. Check out the <color#94c6ff>workshop page</color> for\n" +
+			//				"more information.";
+			label.textScale = 0.6f;
+			label.size = new Vector2(600, 40);
+			label.width = 600;
+			label.relativePosition = new Vector2(40, 40);
+			label.textColor = new Color32(200, 245, 130, 255);
+			label.processMarkup = true;
+			label.eventClicked += TopLabel_eventClicked;
 
-        private void CreateLeftPanel()
-        {
-            offset = new Vector2(4, titleOffset);
+			titleOffset += 50;
+		}
 
-            CreateOptions();
-        }
+		private void CreateLeftPanel()
+		{
+			offset = new Vector2(4, titleOffset);
 
-        private void CreateBottomMessage()
-        {
-            UISprite infoSprite = this.AddUIComponent<UISprite>();
-            infoSprite.spriteName = "IconMessage";
-            infoSprite.size = new Vector2(32, 32);
-            infoSprite.relativePosition = new Vector3(4, height - 40);
+			CreateOptions();
+		}
 
-            UILabel label = this.AddUIComponent<UILabel>();
-            label.text =    "Remember to post your maps to <color#94c6ff>http://reddit.com/r/cimtographer</color>,\n" +
-                            "and show us what you've made!";
-            label.textScale = 0.6f;
-            label.size = new Vector2(400, 40);
-            label.relativePosition = new Vector2(40, height - 32);
-            label.processMarkup = true;
-            label.eventClicked += BottomLabel_eventClicked;
+		private void CreateBottomMessage()
+		{
+			UISprite infoSprite = this.AddUIComponent<UISprite>();
+			infoSprite.spriteName = "IconMessage";
+			infoSprite.size = new Vector2(32, 32);
+			infoSprite.relativePosition = new Vector3(4, height - 40);
 
-            UILabel versionLabel = this.AddUIComponent<UILabel>();
-            versionLabel.text = "v" + MapperOptionsManager.major.ToString() + "." + MapperOptionsManager.minor.ToString() + "." + MapperOptionsManager.build.ToString();
-            versionLabel.textScale = 0.4f;
-            versionLabel.size = new Vector2(40, 15);
-            versionLabel.relativePosition = new Vector2(360, height - 15);
-            versionLabel.processMarkup = true;
-            versionLabel.textColor = new Color32(180, 180, 180, 255);
-            versionLabel.textAlignment = UIHorizontalAlignment.Right;
-            versionLabel.tooltip = versionLabel.text + "." + MapperOptionsManager.revision.ToString();
-        }
+			UILabel label = this.AddUIComponent<UILabel>();
+			label.text = "Remember to post your maps to <color#94c6ff>http://reddit.com/r/cimtographer</color>,\n" +
+							"and show us what you've made!";
+			label.textScale = 0.6f;
+			label.size = new Vector2(400, 40);
+			label.relativePosition = new Vector2(40, height - 32);
+			label.processMarkup = true;
+			label.eventClicked += BottomLabel_eventClicked;
 
-        private void CreateRightPanel()
-        {
-            offset = new Vector2(width / 2, titleOffset);
+			UILabel versionLabel = this.AddUIComponent<UILabel>();
+			versionLabel.text = "v" + MapperOptionsManager.major.ToString() + "." + MapperOptionsManager.minor.ToString() + "." + MapperOptionsManager.build.ToString();
+			versionLabel.textScale = 0.4f;
+			versionLabel.size = new Vector2(40, 15);
+			versionLabel.relativePosition = new Vector2(360, height - 15);
+			versionLabel.processMarkup = true;
+			versionLabel.textColor = new Color32(180, 180, 180, 255);
+			versionLabel.textAlignment = UIHorizontalAlignment.Right;
+			versionLabel.tooltip = versionLabel.text + "." + MapperOptionsManager.revision.ToString();
+		}
 
-            buttonGenerate = CustomUI.UIUtils.CreateButton(this);
-            buttonGenerate.eventClicked += ButtonGenerate_eventClicked;
-            buttonGenerate.textPadding = new RectOffset(6, 6, 6, 6);
-            buttonGenerate.size = new Vector2(190, 30);
-            buttonGenerate.relativePosition = offset;
-            buttonGenerate.text = "Generate OSM map";
+		private void CreateRightPanel()
+		{
+			offset = new Vector2(width / 2, titleOffset);
 
-            offset += new Vector2(0, buttonGenerate.height + 20);
+			buttonGenerate = CustomUI.UIUtils.CreateButton(this);
+			buttonGenerate.eventClicked += ButtonGenerate_eventClicked;
+			buttonGenerate.textPadding = new RectOffset(6, 6, 6, 6);
+			buttonGenerate.size = new Vector2(190, 30);
+			buttonGenerate.relativePosition = offset;
+			buttonGenerate.text = "Generate OSM map";
 
-            UIButton buttonWhatsNew = CustomUI.UIUtils.CreateButton(this);
-            buttonWhatsNew.eventClicked += ButtonWhatsNew_eventClicked;
-            buttonWhatsNew.textPadding = new RectOffset(6, 6, 6, 6);
-            buttonWhatsNew.size = new Vector2(190, 30);
-            buttonWhatsNew.relativePosition = offset;
-            buttonWhatsNew.text = "What's new?";
+			offset += new Vector2(0, buttonGenerate.height + 20);
 
-            offset += new Vector2(0, buttonGenerate.height + 20);
+			UIButton buttonWhatsNew = CustomUI.UIUtils.CreateButton(this);
+			buttonWhatsNew.eventClicked += ButtonWhatsNew_eventClicked;
+			buttonWhatsNew.textPadding = new RectOffset(6, 6, 6, 6);
+			buttonWhatsNew.size = new Vector2(190, 30);
+			buttonWhatsNew.relativePosition = offset;
+			buttonWhatsNew.text = "What's new?";
 
-            UILabel label = this.AddUIComponent<UILabel>();
-            label.text =    "<color#94c6ff>Where's the import gone?</color>\n" +
-                            "Sorry, but the import functionality\n" +
-                            "is something that needs re-developing,\n" +
-                            "and doesn't currently work with the\n" +
-                            "new code. Since it was the same\n" +
-                            "version as the old Cimtographer mod\n" +
-                            "you could still use that mod to\n" +
-                            "import for now.\n\n" +
+			offset += new Vector2(0, buttonGenerate.height + 20);
 
-                            "<color#94c6ff>Issues?</color>\n" +
-                            "If there's any problems with this\n" +
-                            "version of the mod, let me know on\n" +
-                            "either Reddit or the Workshop.";
-            label.textScale = 0.6f;
-            label.size = new Vector2(200, 40);
-            label.relativePosition = offset;
-            label.processMarkup = true;
-        }
+			UILabel label = this.AddUIComponent<UILabel>();
+			//label.text = "<color#94c6ff>Where's the import gone?</color>\n" +
+			//				"Sorry, but the import functionality\n" +
+			//				"is something that needs re-developing,\n" +
+			//				"and doesn't currently work with the\n" +
+			//				"new code. Since it was the same\n" +
+			//				"version as the old Cimtographer mod\n" +
+			//				"you could still use that mod to\n" +
+			//				"import for now.\n\n" +
 
-        private void CreateOptions()
-        {
-            scrollOptionsList = UIFastList.Create<UIOptionItem>(this);
-            scrollOptionsList.backgroundSprite = "UnlockingPanel";
-            scrollOptionsList.size = new Vector2(192, 250);
-            scrollOptionsList.canSelect = true;
-            scrollOptionsList.relativePosition = offset;
-            scrollOptionsList.rowHeight = 20f;
-            scrollOptionsList.rowsData.Clear();
-            scrollOptionsList.selectedIndex = -1;
+			//				"<color#94c6ff>Issues?</color>\n" +
+			//				"If there's any problems with this\n" +
+			//				"version of the mod, let me know on\n" +
+			//				"either Reddit or the Workshop.";
+			label.textScale = 0.6f;
+			label.size = new Vector2(200, 40);
+			label.relativePosition = offset;
+			label.processMarkup = true;
+		}
 
-            offset += new Vector2(0, scrollOptionsList.height + 5);
+		private void CreateOptions()
+		{
+			scrollOptionsList = UIFastList.Create<UIOptionItem>(this);
+			scrollOptionsList.backgroundSprite = "UnlockingPanel";
+			scrollOptionsList.size = new Vector2(192, 250);
+			scrollOptionsList.canSelect = true;
+			scrollOptionsList.relativePosition = offset;
+			scrollOptionsList.rowHeight = 20f;
+			scrollOptionsList.rowsData.Clear();
+			scrollOptionsList.selectedIndex = -1;
 
-            CreateOptionList(MapperOptionsManager.exportOptions, scrollOptionsList);
+			offset += new Vector2(0, scrollOptionsList.height + 5);
 
-            scrollOptionsList.DisplayAt(0);
-            scrollOptionsList.selectedIndex = 0;
-        }
+			CreateOptionList(MapperOptionsManager.exportOptions, scrollOptionsList);
 
-        private void CreateOptionList(Dictionary<string, OptionItem> options, UIFastList list = null)
-        {
-            if (list != null)
-            {
-                foreach (KeyValuePair<string, OptionItem> option in options)
-                {
-                    option.Value.id = option.Key;
+			scrollOptionsList.DisplayAt(0);
+			scrollOptionsList.selectedIndex = 0;
+		}
 
-                    list.rowsData.Add(option.Value);
-                }
-            }
-        }
+		private void CreateOptionList(Dictionary<string, OptionItem> options, UIFastList list = null)
+		{
+			if (list != null)
+			{
+				foreach (KeyValuePair<string, OptionItem> option in options)
+				{
+					option.Value.id = option.Key;
 
-        private void ButtonGenerate_eventClicked(UIComponent component, UIMouseEventParameter eventParam)
-        {
-            try
-            {
-                OSMExportNew osmExporter = new OSMExportNew();
-                osmExporter.Export();
+					list.rowsData.Add(option.Value);
+				}
+			}
+		}
 
-                buttonGenerate.text = "Generate OSM map";
-            }
-            catch
-            {
-                buttonGenerate.text = "Export failed!";
-            }
-        }
+		private void ButtonGenerate_eventClicked(UIComponent component, UIMouseEventParameter eventParam)
+		{
+			try
+			{
+				OSMExportNew osmExporter = new OSMExportNew();
+				osmExporter.Export();
 
-        private void ButtonWhatsNew_eventClicked(UIComponent component, UIMouseEventParameter eventParam)
-        {
-            if(whatsNewPanel != null)
-            {
-                whatsNewPanel.Show();
-                whatsNewPanel.BringToFront();
-            }
-        }
+				buttonGenerate.text = "Generate OSM map";
+			}
+			catch
+			{
+				buttonGenerate.text = "Export failed!";
+			}
+		}
 
-        private void TopLabel_eventClicked(UIComponent component, UIMouseEventParameter eventParam)
-        {
-            Steam.ActivateGameOverlayToWorkshopItem(new PublishedFileId(549792340));
-        }
+		private void ButtonWhatsNew_eventClicked(UIComponent component, UIMouseEventParameter eventParam)
+		{
+			if (whatsNewPanel != null)
+			{
+				whatsNewPanel.Show();
+				whatsNewPanel.BringToFront();
+			}
+		}
 
-        private void BottomLabel_eventClicked(UIComponent component, UIMouseEventParameter eventParam)
-        {
-            Steam.ActivateGameOverlayToWebPage("https://www.reddit.com/r/Cimtographer");
-        }
-    }
+		private void TopLabel_eventClicked(UIComponent component, UIMouseEventParameter eventParam)
+		{
+			//Steam.ActivateGameOverlayToWorkshopItem(new PublishedFileId(549792340));
+		}
+
+		private void BottomLabel_eventClicked(UIComponent component, UIMouseEventParameter eventParam)
+		{
+			//Steam.ActivateGameOverlayToWebPage("https://www.reddit.com/r/Cimtographer");
+		}
+	}
 }
